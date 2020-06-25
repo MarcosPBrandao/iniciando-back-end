2891be8f2be5ecf40b40fc92df126689f6efb452
MarcosPBrandao <brandaomarcosp@gmail.com>
2020-06-25 18:20:06 -0300

    Commit Upload de Imagens

--- src/models/Users.ts
+++ src/models/Users.ts
@@ -13,6 +13,9 @@ class Users {
     @Column()
     password: string;
     
+    @Column()
+    avatar: string;
+
     @CreateDateColumn()
     created_at: Date;
 
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
-const usersRouter = Router();    
+import updateUserAvatarService from '../services/UpdateUserAvatarService';
+import ensureAuthenticates from '../middlewares/EnsureAuthenticated'
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+const usersRouter = Router();   
+const upload = multer(uploadConfig); 
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -23,5 +29,24 @@ usersRouter.post('/', async (request, response) => {
     }
  
 })
+
+usersRouter.patch(
+   '/avatar', 
+   ensureAuthenticates, 
+   upload.single('avatar'), 
+   async (request, response) => {
+   try {
+      const updateUserAvatarService = new UpdateUserAvatarService(); 
+      const user = await updateUserAvatarService.execute({
+          user_id: request.user.id,
+          avatarFilename: request.file.filename
+      })
+      delete user.password;
+      return response.json(user); 
+   } catch (err) {
+     return response.status(400).json({ error: err.message });  
+   }
+   
+});
 
 export default usersRouter;
